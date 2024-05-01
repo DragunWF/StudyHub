3c44c9c43143b2047884f8073108f0b7a425572e
dragunwf <plarisanmarc2@gmail.com>
2024-05-01 19:24:08 +0800

    Update LandingPage.java Implement functionality

--- app/src/main/java/com/example/studyhub/LandingPage.java
+++ app/src/main/java/com/example/studyhub/LandingPage.java
@@ -1,7 +1,9 @@
 package com.example.studyhub;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LandingPage extends AppCompatActivity {
+    private Button loginBtn;
+    private Button registerBtn;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -17,5 +21,15 @@ public class LandingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_landing_page);
+
+        loginBtn = findViewById(R.id.landingLogInBtn);
+        registerBtn = findViewById(R.id.landingSignUpBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            startActivity(new Intent(LandingPage.this, LogInActivity.class));
+        });
+        registerBtn.setOnClickListener(v -> {
+            startActivity(new Intent(LandingPage.this, SignUpActivity.class));
+        });
     }
 }
