5a8be7c4797a7ae3ae6ff85cacb1deaa078be4ee
dragunwf <plarisanmarc2@gmail.com>
2024-05-01 19:25:06 +0800

    Update SignUpActivity.java and activity_sign_up.xml Implement input validation and button functionality. Rename one UI id

--- app/src/main/java/com/example/studyhub/SignUpActivity.java
+++ app/src/main/java/com/example/studyhub/SignUpActivity.java
@@ -1,6 +1,10 @@
 package com.example.studyhub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.studyhub.utils.Utils;
+
 public class SignUpActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+
+    private EditText firstName;
+    private EditText lastName;
+
+    private EditText course;
+    private EditText email;
+    private EditText mobileNumber;
+
+    private RadioButton studentBtn;
+    private RadioButton tutorBtn;
+
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,68 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        username = findViewById(R.id.signUpInputUsername);
+        password = findViewById(R.id.signUpInputPassword);
+
+        firstName = findViewById(R.id.signUpInputFirstName);
+        lastName = findViewById(R.id.signUpInputLastName);
+
+        course = findViewById(R.id.signUpInputCourse);
+        email = findViewById(R.id.signUpInputEmail);
+        mobileNumber = findViewById(R.id.signUpInputMobileNo);
+
+        studentBtn = findViewById(R.id.rBtnStudent);
+        tutorBtn = findViewById(R.id.rBtnTutor);
+        signUpBtn = findViewById(R.id.signUpBtn);
+
+        signUpBtn.setOnClickListener(v -> {
+            if (isInputValid()) {
+                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private boolean isInputValid() {
+        final int PASSWORD_CHAR_LIMIT = 8;
+
+        if (Utils.getString(username).isEmpty()) {
+            toast("Username cannot be empty!");
+            return false;
+        }
+        if (Utils.getString(password).length() < PASSWORD_CHAR_LIMIT) {
+            toast(String.format("Password must at least be %s characters", PASSWORD_CHAR_LIMIT));
+            return false;
+        }
+        if (Utils.getString(firstName).isEmpty()) {
+            toast("First name cannot be empty!");
+            return false;
+        }
+        if (Utils.getString(lastName).isEmpty()) {
+            toast("Last name cannot be empty");
+            return false;
+        }
+        if (Utils.getString(course).isEmpty()) {
+            toast("Course cannot be empty!");
+            return false;
+        }
+        if (Utils.getString(email).isEmpty()) {
+            toast("Email cannot be empty!");
+            return false;
+        }
+        if (Utils.getString(mobileNumber).isEmpty()) {
+            toast("Mobile Number cannot be empty!");
+            return false;
+        }
+        if (!studentBtn.isChecked() && !tutorBtn.isChecked()) {
+            toast("Please select an account type! Either Student or Tutor.")
+            return false;
+        }
+
+        return true;
+    }
+
+    private void toast(String message) {
+        Utils.toast(this, message);
     }
 }
