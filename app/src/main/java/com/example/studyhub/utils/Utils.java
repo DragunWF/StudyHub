f4be79a768ff96c51a40b9cbedd6a169d310814c
dragunwf <plarisanmarc2@gmail.com>
2024-05-01 19:23:56 +0800

    Update Utils.java Add new method getString() for directly getting the string text from EditText objects

--- app/src/main/java/com/example/studyhub/utils/Utils.java
+++ app/src/main/java/com/example/studyhub/utils/Utils.java
@@ -1,10 +1,15 @@
 package com.example.studyhub.utils;
 
 import android.content.Context;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Utils {
     public static void toast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static String getString(EditText text) {
+        return String.valueOf(text.getText());
     }
 }
