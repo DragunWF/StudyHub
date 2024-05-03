9d95d72dc7f7cba26604311cdac5453e5c5a4b3f
dragunwf <plarisanmarc2@gmail.com>
2024-05-03 11:10:31 +0800

    Update Dashboard activity

--- app/src/main/java/com/example/studyhub/activities/DashboardActivity.java
+++ app/src/main/java/com/example/studyhub/activities/DashboardActivity.java
@@ -1,16 +1,37 @@
 package com.example.studyhub.activities;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studyhub.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DashboardActivity extends AppCompatActivity {
+    private ImageView profileIcon;
+    private SearchView searchBar;
+
+    private Spinner userTypeSpinner;
+    private List<String> userTypeChoices = Arrays.asList("Student", "Tutor");
+
+    private Spinner courseSpinner;
+    private List<String> courseChoices = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
+    private RecyclerView.Adapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +43,30 @@ public class DashboardActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        profileIcon = findViewById(R.id.profileIconDashboard);
+        searchBar = findViewById(R.id.searchBarDashboard);
+
+        profileIcon.setOnClickListener(v -> {
+            // startActivity(DashboardActivity.this, ProfileActivity.class);
+        });
+
+        setRecyclerView();
+        setSpinners();
+    }
+
+    private void setRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerViewDashboard);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+
+        // recyclerAdapter =
+    }
+
+    private void setSpinners() {
+        userTypeSpinner = findViewById(R.id.userTypeSpinner);
+        courseSpinner = findViewById(R.id.courseSpinner);
     }
 }
