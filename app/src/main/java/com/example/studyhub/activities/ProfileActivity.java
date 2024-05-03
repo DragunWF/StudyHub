df4a1a13dc63ed9053b0a395dc7c5cbb940bc620
dragunwf <plarisanmarc2@gmail.com>
2024-05-03 22:49:39 +0800

    Update Profile activity (Java and XML)

--- app/src/main/java/com/example/studyhub/activities/ProfileActivity.java
+++ app/src/main/java/com/example/studyhub/activities/ProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.studyhub.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.studyhub.R;
 
 public class ProfileActivity extends AppCompatActivity {
+    private ImageView backBtn;
+    private ImageView viewBuddyListBtn;
+    private ImageView editProfileBtn;
+    private ImageView subscriptionBtn;
+
+    private TextView description;
+    private TextView username;
+    private TextView fullName;
+
+    private TextView course;
+    private TextView accType;
+    private TextView email;
+    private TextView mobileNumber;
+
+    private TextView buddyCount;
+    private TextView accTier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +40,40 @@ public class ProfileActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        description = findViewById(R.id.outputProfileDesc);
+        username = findViewById(R.id.outputProfileDetailsUsername);
+        fullName = findViewById(R.id.outputProfileDetailsFullName);
+
+        course = findViewById(R.id.outputProfileDetailsCourse);
+        accType = findViewById(R.id.outputProfileDetailsAccType);
+        email = findViewById(R.id.outputProfileDetailsEmail);
+        mobileNumber = findViewById(R.id.outputProfileDetailsMobileNo);
+
+        buddyCount = findViewById(R.id.outputProfileBuddyCount);
+        accTier = findViewById(R.id.outputProfileAccountTier);
+
+        setButtons();
+    }
+
+    private void setButtons() {
+        backBtn = findViewById(R.id.profileBackBtn);
+        viewBuddyListBtn = findViewById(R.id.viewBuddyListBtn);
+        editProfileBtn = findViewById(R.id.editProfileBtn);
+        subscriptionBtn = findViewById(R.id.subscriptionBtn);
+
+        backBtn.setOnClickListener(v -> {
+            finish();
+        });
+        viewBuddyListBtn.setOnClickListener(v -> {
+            // startActivity(new Intent(ProfileActivity.this, BuddyListActivity.class));
+        });
+        editProfileBtn.setOnClickListener(v -> {
+            // startActivity(new Intent(ProfileActivity.this, AccountEditActivity.class));
+        });
+        subscriptionBtn.setOnClickListener(v -> {
+           // startActivity(new Intent(ProfileActivity.this, SubscriptionActivity.class));
         });
     }
 }
