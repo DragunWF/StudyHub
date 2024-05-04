08d9f819c0b02a8abc87ebe138559afc65e79732
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 13:29:55 +0800

    Update RequestViewProfileActivity.java Implement data setting and button functionality

--- app/src/main/java/com/example/studyhub/activities/RequestViewProfileActivity.java
+++ app/src/main/java/com/example/studyhub/activities/RequestViewProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.studyhub.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.studyhub.R;
+import com.example.studyhub.data.User;
 
 public class RequestViewProfileActivity extends AppCompatActivity {
+    private ImageView backBtn;
+    private ImageView acceptBtn;
+    private ImageView rejectBtn;
+
+    private TextView usernameHeader;
+    private TextView description;
+
+    private TextView username;
+    private TextView fullName;
+
+    private TextView course;
+    private TextView accType;
+    private TextView email;
+    private TextView mobileNumber;
+
+    private User viewedUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +42,35 @@ public class RequestViewProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void setButtons() {
+        backBtn = findViewById(R.id.requestViewProfileBackBtn);
+        acceptBtn = findViewById(R.id.acceptRequestBtn);
+        rejectBtn = findViewById(R.id.rejectRequestBtn);
+
+        backBtn.setOnClickListener(v -> {
+            finish();
+        });
+        acceptBtn.setOnClickListener(v -> {
+            // TODO: Implement accept request functionality
+        });
+        rejectBtn.setOnClickListener(v -> {
+            // TODO: Implement reject request functionality
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setInfo() {
+        usernameHeader.setText(viewedUser.getUsername());
+        description.setText(viewedUser.getDescription());
+
+        username.setText("Username: " + viewedUser.getUsername());
+        fullName.setText("Full Name: " + viewedUser.getFullName());
+
+        course.setText("Course: " + viewedUser.getCourse());
+        accType.setText("Acc. Type: " + viewedUser.getUserType());
+        email.setText("Email: " + viewedUser.getEmail());
+        mobileNumber.setText("Mobile No: " + viewedUser.getMobileNumber());
     }
 }
