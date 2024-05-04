75656ab182ff95ce7f62f7997c231da8190a9661
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 23:00:43 +0800

    Update Utils.java - Implement random phone number generation and user values query method

--- app/src/main/java/com/example/studyhub/utils/Utils.java
+++ app/src/main/java/com/example/studyhub/utils/Utils.java
@@ -23,6 +23,35 @@ public class Utils {
         return Utils.getString(text).isEmpty();
     }
 
+    public static String generateRandomPhoneNumber() {
+        StringBuilder output = new StringBuilder();
+        output.append("0960");
+        // 7 to indicate the remaining numbers for a complete phone number
+        // This method assumes all phone numbers come from the Philippines therefore +63 isn't needed
+        for (int i = 1; i <= 7; i++) {
+            output.append((int)Math.floor(Math.random() * 10));
+        }
+        return output.toString();
+    }
+
+    public static String generateUserValuesQuery(User[] users) {
+        StringBuilder query = new StringBuilder();
+        query.append("VALUES ");
+        for (int i = 0 ; i < users.length; i++) {
+            User user = users[i];
+            // username, password, firstName, lastName, course
+            // email, mobileNumber, userType, description, subscriptionId
+            String row = String.format("('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s)",
+                    user.getUsername(), user.getPassword(), user.getFirstName(), user.getLastName(), user.getCourse(),
+                    user.getEmail(), user.getMobileNumber(), user.getUserType(), user.getDescription(), user.getSubscriptionId());
+            if (i + 1 != users.length) {
+                row += ", ";
+            }
+            query.append(row);
+        }
+        return query.toString();
+    }
+
     @SuppressLint("SetTextI18n")
     public static void setProfileInfo(User user, HashMap<String, TextView> infoMap) {
         try {
