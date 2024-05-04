59d460e197ca48b720cf6a6b86d87760d48e9d9e
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 12:08:50 +0800

    Move activity Java files to activities package

--- app/src/main/java/com/example/studyhub/activities/RequestListActivity.java
+++ app/src/main/java/com/example/studyhub/activities/RequestListActivity.java
@@ -1,4 +1,4 @@
-package com.example.studyhub;
+package com.example.studyhub.activities;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class BuddyListActivity extends AppCompatActivity {
+import com.example.studyhub.R;
+
+public class RequestListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_buddy_list);
+        setContentView(R.layout.activity_request_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
