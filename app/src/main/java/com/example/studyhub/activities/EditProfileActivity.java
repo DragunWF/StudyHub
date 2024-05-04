41ccb730bb31a32654af1e41d7ee0ef32ed161c6
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 12:58:28 +0800

    Update EditProfileActivity.java (Java and XML) UI bug fixes, back-end functionality implementation

--- app/src/main/java/com/example/studyhub/activities/EditProfileActivity.java
+++ app/src/main/java/com/example/studyhub/activities/EditProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.studyhub.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,19 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.studyhub.R;
 
 public class EditProfileActivity extends AppCompatActivity {
+    private ImageView backBtn;
+    private Button submitBtn;
+
+    private TextView username;
+    private TextView password;
+    private TextView firstName;
+    private TextView lastName;
+
+
+    private TextView course;
+    private TextView email;
+    private TextView mobileNumber;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +38,32 @@ public class EditProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        username = findViewById(R.id.inputEditProfileUsername);
+        firstName = findViewById(R.id.inputEditProfileFirstname);
+        lastName = findViewById(R.id.inputEditProfileLastname);
+
+        course = findViewById(R.id.inputEditProfileCourse);
+        email = findViewById(R.id.inputEditProfileEmail);
+        mobileNumber = findViewById(R.id.inputEditProfileMobileNo);
+        description = findViewById(R.id.inputEditProfileDesc);
+
+        setButtons();
+    }
+
+    private void setButtons() {
+        backBtn = findViewById(R.id.editProfileBackBtn);
+        submitBtn = findViewById(R.id.editProfileSubmitBtn);
+
+        backBtn.setOnClickListener(v -> {
+            finish();
+        });
+        submitBtn.setOnClickListener(v -> {
+            // TODO: Implement account information changes
+        });
+    }
+
+    private void isInputValid() {
+
     }
 }
