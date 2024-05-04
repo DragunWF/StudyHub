73d89779fd8f3be59655f97e9ded82ad20cc31d3
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 13:39:51 +0800

    Update Utils.java Implement setProfileInfo() method utilizing a HashMap

--- app/src/main/java/com/example/studyhub/utils/Utils.java
+++ app/src/main/java/com/example/studyhub/utils/Utils.java
@@ -1,8 +1,14 @@
 package com.example.studyhub.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.studyhub.data.User;
+
+import java.util.HashMap;
 
 public class Utils {
     public static void toast(Context context, String message) {
@@ -15,5 +21,23 @@ public class Utils {
 
     public static boolean isEmpty(EditText text) {
         return Utils.getString(text).isEmpty();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public static void setProfileInfo(User user, HashMap<String, TextView> infoMap) {
+        try {
+            infoMap.get("description").setText(user.getDescription());
+            infoMap.get("username").setText("Username: " + user.getUsername());
+            infoMap.get("fullName").setText("Full Name: " + user.getFullName());
+            infoMap.get("course").setText("Course: " + user.getCourse());
+            infoMap.get("userType").setText("Acc. Type: " + user.getUserType());
+            infoMap.get("email").setText("Email: " + user.getEmail());
+            infoMap.get("mobileNumber").setText("Mobile No: " + user.getMobileNumber());
+        } catch (NullPointerException error) {
+            infoMap.get("description").setText("Error trying to retrieve user data!");
+            for (String key : infoMap.keySet()) {
+                infoMap.get(key).setText("Null");
+            }
+        }
     }
 }
