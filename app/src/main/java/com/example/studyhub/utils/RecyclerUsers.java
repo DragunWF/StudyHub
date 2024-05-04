e6d491546fb38dad91a80e6621d2189492dd1615
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 13:15:15 +0800

    Update RecyclerUsers.java Implement functionality for recycler view adapter

--- app/src/main/java/com/example/studyhub/utils/RecyclerUsers.java
+++ app/src/main/java/com/example/studyhub/utils/RecyclerUsers.java
@@ -1,15 +1,18 @@
 package com.example.studyhub.utils;
 
-
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studyhub.R;
+import com.example.studyhub.activities.ViewOtherProfileActivity;
 import com.example.studyhub.data.User;
 
 import java.util.List;
@@ -23,17 +26,23 @@ public class RecyclerUsers extends RecyclerView.Adapter<RecyclerUsers.ViewHolder
      * (custom ViewHolder)
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private final TextView name;
+        private final ImageView viewProfileBtn;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
 
-            textView = (TextView) view.findViewById(R.id.textView);
+            name = view.findViewById(R.id.outputBuddyListName);
+            viewProfileBtn = view.findViewById(R.id.buddyViewProfileBtn);
         }
 
-        public TextView getTextView() {
-            return textView;
+        public TextView getName() {
+            return name;
+        }
+
+        public ImageView getViewProfileBtn() {
+            return viewProfileBtn;
         }
     }
 
@@ -59,11 +68,18 @@ public class RecyclerUsers extends RecyclerView.Adapter<RecyclerUsers.ViewHolder
     }
 
     // Replace the contents of a view (invoked by the layout manager)
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-
+        User user = userList.get(position);
+        viewHolder.getName().setText("Name: " + user.getFullName());
+        viewHolder.getViewProfileBtn().setOnClickListener(v -> {
+            Intent intent = new Intent(context, ViewOtherProfileActivity.class);
+            intent.putExtra("id", user.getId());
+            context.startActivity(intent);
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
