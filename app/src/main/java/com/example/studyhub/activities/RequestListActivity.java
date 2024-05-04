91a28212ed179b459d515df4bac495300b9c9ed5
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 13:22:43 +0800

    Update RequestListActivity.java Implement data setting and button functionality

--- app/src/main/java/com/example/studyhub/activities/RequestListActivity.java
+++ app/src/main/java/com/example/studyhub/activities/RequestListActivity.java
@@ -1,16 +1,33 @@
 package com.example.studyhub.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studyhub.R;
+import com.example.studyhub.data.User;
+import com.example.studyhub.utils.RecyclerUsers;
+
+import java.util.List;
 
 public class RequestListActivity extends AppCompatActivity {
+    private ImageView backBtn;
+    private Button homeBtn;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
+    private RecyclerView.Adapter recyclerAdapter;
+
+    private List<User> userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,34 @@ public class RequestListActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // TODO: Implement data retrieval to fill userList
+
+        setRecyclerView();
+        setButtons();
+    }
+
+    private void setRecyclerView() {
+        recyclerView = findViewById(R.id.requestListRecyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+
+        recyclerAdapter = new RecyclerUsers(this, userList);
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+    private void setButtons() {
+        backBtn = findViewById(R.id.requestListBackBtn);
+        homeBtn = findViewById(R.id.requestListHomeBtn);
+
+        backBtn.setOnClickListener(v -> {
+            finish();
+        });
+        homeBtn.setOnClickListener(v -> {
+            startActivity(new Intent(RequestListActivity.this, DashboardActivity.class));
         });
     }
 }
