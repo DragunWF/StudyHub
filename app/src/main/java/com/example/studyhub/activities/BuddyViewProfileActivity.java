9596bbe9b5815dbcc58ebbef5a609bad63e39653
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 12:23:42 +0800

    Update BuddyViewProfileActivity.java

--- app/src/main/java/com/example/studyhub/activities/BuddyViewProfileActivity.java
+++ app/src/main/java/com/example/studyhub/activities/BuddyViewProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.studyhub.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.studyhub.R;
+import com.example.studyhub.data.User;
 
 public class BuddyViewProfileActivity extends AppCompatActivity {
+    private ImageView backBtn;
+    private TextView buddyCount;
+
+    private TextView usernameHeader;
+    private TextView description;
+
+    private TextView username;
+    private TextView fullName;
+
+    private TextView course;
+    private TextView accType;
+    private TextView email;
+    private TextView mobileNumber;
+
+    private User buddy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +41,41 @@ public class BuddyViewProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // TODO: Implement buddy data retrieval
+
+        backBtn = findViewById(R.id.buddyViewProfileBackBtn);
+        buddyCount = findViewById(R.id.outputBuddyViewProfileSBC);
+
+        usernameHeader = findViewById(R.id.outputBuddyViewProfileHeaderUsername);
+        description = findViewById(R.id.outputBuddyViewProfileDesc);
+
+        username = findViewById(R.id.outputBuddyViewProfileUsername);
+        fullName = findViewById(R.id.outputBuddyViewProfileFullname);
+
+        course = findViewById(R.id.outputBuddyViewProfileCourse);
+        accType = findViewById(R.id.outputBuddyViewProfileAccType);
+        email = findViewById(R.id.outputBuddyViewProfileEmail);
+        mobileNumber = findViewById(R.id.outputBuddyViewProfileMobileNo);
+
+        backBtn.setOnClickListener(v -> {
+            finish();
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setInfo() {
+        usernameHeader.setText(buddy.getUsername());
+        description.setText(buddy.getDescription());
+
+        username.setText("Username: " + buddy.getUsername());
+        fullName.setText("Full Name: " + buddy.getFullName());
+
+        course.setText("Course: " + buddy.getCourse());
+        accType.setText("Acc. Type: " + buddy.getUserType());
+        email.setText("Email: " + buddy.getEmail());
+        mobileNumber.setText("Mobile No: " + buddy.getMobileNumber());
+
+        // TODO: Implement buddy counting
     }
 }
