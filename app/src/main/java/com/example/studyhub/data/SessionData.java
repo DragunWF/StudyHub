7649b797c172dbee4172dbb6284ad15f96d9e576
dragunwf <plarisanmarc2@gmail.com>
2024-05-04 12:33:25 +0800

    Update SessionData.java Add data for the current logged in user

--- app/src/main/java/com/example/studyhub/data/SessionData.java
+++ app/src/main/java/com/example/studyhub/data/SessionData.java
@@ -1,4 +1,9 @@
 package com.example.studyhub.data;
 
 public class SessionData {
+    private static User currentUser;
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
 }
