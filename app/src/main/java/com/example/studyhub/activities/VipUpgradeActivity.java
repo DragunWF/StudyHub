16c937c6d26031bce3ebaa2be05f82fbca1339b9
papsykle <toramgarcia@gmail.com>
2024-05-11 23:19:18 +0800

    naming fix

--- app/src/main/java/com/example/studyhub/activities/VipUpgradeActivity.java
+++ app/src/main/java/com/example/studyhub/activities/VipUpgradeActivity.java
@@ -1,6 +1,8 @@
 package com.example.studyhub.activities;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +13,13 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.studyhub.R;
 
 public class VipUpgradeActivity extends AppCompatActivity {
+    private RadioButton monthlyRbtn;
+    private RadioButton yearlyRbtn;
+    private RadioButton gcashRbtn;
+    private RadioButton mayaRbtn;
+    private RadioButton cardRbtn;
+    private Button subscribeBtn;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -22,5 +29,9 @@ public class VipUpgradeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        monthlyRbtn = findViewById(R.id.subsRBtnMonthly);
+        yearlyRbtn = findViewById(R.id.subsRBtnYearly);
+        gcashRbtn = findViewById(R.id.subsRBtnGcash);
+
     }
 }
