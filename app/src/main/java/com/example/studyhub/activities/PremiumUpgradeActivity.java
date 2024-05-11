9299c515e88f74a28e6f381f9a82f8a0a94312fa
dragunwf <plarisanmarc2@gmail.com>
2024-05-11 22:46:40 +0800

    Update PremiumUpgradeActivity.java Checkpoint

--- app/src/main/java/com/example/studyhub/activities/PremiumUpgradeActivity.java
+++ app/src/main/java/com/example/studyhub/activities/PremiumUpgradeActivity.java
@@ -1,6 +1,9 @@
 package com.example.studyhub.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,15 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.studyhub.R;
 
 public class PremiumUpgradeActivity extends AppCompatActivity {
+    private ImageView backBtn;
+    private Button subscribeBtn;
+
+    private RadioButton gCashBtn;
+    private RadioButton mayaBtn;
+    private RadioButton cardBtn;
+
+    private RadioButton monthlyBtn;
+    private RadioButton yearlyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,21 @@ public class PremiumUpgradeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        backBtn = findViewById(R.id.premiumBackBtn);
+        subscribeBtn = findViewById(R.id.subsPremiumBtn);
+
+        gCashBtn = findViewById(R.id.subsRBtnGcash1);
+        mayaBtn = findViewById(R.id.subsRBtnMaya1);
+        cardBtn = findViewById(R.id.subsRBtnCard1);
+
+        monthlyBtn = findViewById(R.id.subsRBtnMonthly1);
+        yearlyBtn = findViewById(R.id.subsRBtnYearly1);
+
+        backBtn.setOnClickListener(v -> finish());
+        subscribeBtn.setOnClickListener(v -> {
+            // TODO: Implement input validation
         });
     }
 }
