14a048074c4d180c63b5e69a4003240739d84f42
dragunwf <plarisanmarc2@gmail.com>
2024-05-11 22:33:40 +0800

    Move new pages to activities package

--- app/src/main/java/com/example/studyhub/activities/PremiumUpgradeActivity.java
+++ app/src/main/java/com/example/studyhub/activities/PremiumUpgradeActivity.java
@@ -1,4 +1,4 @@
-package com.example.studyhub;
+package com.example.studyhub.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.studyhub.R;
 
 public class PremiumUpgradeActivity extends AppCompatActivity {
 
