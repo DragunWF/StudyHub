fdd2f8f541504d8637023f60c1d8f51059f21be8
dragunwf <plarisanmarc2@gmail.com>
2024-05-26 19:48:47 +0800

    Update BuddyListActivity.java

--- app/src/main/java/com/example/studyhub/activities/BuddyListActivity.java
+++ app/src/main/java/com/example/studyhub/activities/BuddyListActivity.java
@@ -1,16 +1,33 @@
 package com.example.studyhub.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studyhub.R;
+import com.example.studyhub.data.User;
+import com.example.studyhub.utils.RecyclerUsers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BuddyListActivity extends AppCompatActivity {
+    private ImageView backBtn;
+    private Button homeBtn;
+
+    private RecyclerView buddyRecycler;
+    private RecyclerView.Adapter adapter;
+
+    private List<User> buddies = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +39,30 @@ public class BuddyListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        backBtn = findViewById(R.id.buddyListBackBtn);
+        homeBtn = findViewById(R.id.buddyListHomeBtn);
+
+        backBtn.setOnClickListener(v -> finish());
+        homeBtn.setOnClickListener(v -> {
+            startActivity(new Intent(BuddyListActivity.this, DashboardActivity.class));
+        });
+
+        setBuddiesData();
+        setRecyclerView();
+    }
+
+    private void setBuddiesData() {
+        // TODO: Implement buddy data grabbing
+    }
+
+    private void setRecyclerView() {
+        buddyRecycler = findViewById(R.id.buddyListRecyclerView);
+
+        buddyRecycler.setHasFixedSize(true);
+        buddyRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new RecyclerUsers(this, buddies);
+        buddyRecycler.setAdapter(adapter);
     }
 }
